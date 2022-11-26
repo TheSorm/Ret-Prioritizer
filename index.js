cd6664f64730c54680bdb70c510362845e8213ec
TheSorm <thesorm@web.de>
2022-11-26 23:35:45 +0100

    Init

---
+++ index.js
@@ -0,0 +1,98 @@
+const Ability = {
+	Judgement: "Judgement",
+	CrusaderStrike: "CrusaderStrike",
+	DivineStorm: "DivineStorm",
+	HammerOfWrath: "HammerOfWrath",
+	Exorcism: "Exorcism",
+	Consecration: "Consecration",
+	HolyWrath: "HolyWrath"
+}
+
+const ms = 1
+const s = 1000
+const gcd = 1500
+
+function findBestPrio() {
+	const spellCDs = new Map();
+	const spellDmgs = new Map();
+	const spellGcds = new Map();
+	const abilitys = [];
+	
+	let righteousVengeanceDmg = 0
+	if (document.getElementById("RighteousVengeanceEnabled").checked){
+		righteousVengeanceDmg = document.getElementById("RighteousVengeanceAvrCast").valueAsNumber;
+	}
+	
+	let sealOfVengeanceDmg = 0
+	if (document.getElementById("SealOfVengeanceEnabled").checked){
+		sealOfVengeanceDmg = document.getElementById("SealOfVengeanceAvrCast").valueAsNumber;
+	}
+	
+	if (document.getElementById("JudgementEnabled").checked) {
+		abilitys.push(Ability.Judgement);
+		spellCDs.set(Ability.Judgement, document.getElementById("JudgementCD").valueAsNumber * s);
+		spellDmgs.set(Ability.Judgement, document.getElementById("JudgementAvrCast").valueAsNumber);
+		spellGcds.set(Ability.Judgement, gcd);
+	}
+	
+	if (document.getElementById("CrusaderStrikeEnabled").checked) {
+		abilitys.push(Ability.CrusaderStrike);
+		spellCDs.set(Ability.CrusaderStrike, document.getElementById("CrusaderStrikeCD").valueAsNumber * s);
+		let avrCast = document.getElementById("CrusaderStrikeAvrCast").valueAsNumber
+		let crit = document.getElementById("CrusaderStrikeCrit").valueAsNumber / 100
+		spellDmgs.set(Ability.CrusaderStrike, avrCast + sealOfVengeanceDmg + (righteousVengeanceDmg * crit) );
+		spellGcds.set(Ability.CrusaderStrike, gcd);
+	}
+	
+	if (document.getElementById("DivineStormEnabled").checked) {
+		abilitys.push(Ability.DivineStorm);
+		spellCDs.set(Ability.DivineStorm, document.getElementById("DivineStormCD").valueAsNumber * s);
+		let avrCast = document.getElementById("DivineStormAvrCast").valueAsNumber;
+		let crit = document.getElementById("DivineStormCrit").valueAsNumber / 100;
+		spellDmgs.set(Ability.DivineStorm, avrCast + sealOfVengeanceDmg + (righteousVengeanceDmg * crit) );
+		spellGcds.set(Ability.DivineStorm, gcd);
+	}
+	
+	if (document.getElementById("HammerOfWrathEnabled").checked) {
+		abilitys.push(Ability.HammerOfWrath);
+		spellCDs.set(Ability.HammerOfWrath, document.getElementById("HammerOfWrathCD").valueAsNumber * s);
+		let avrCast = document.getElementById("HammerOfWrathAvrCast").valueAsNumber;
+		let crit = document.getElementById("HammerOfWrathCrit").valueAsNumber / 100;
+		spellDmgs.set(Ability.HammerOfWrath, avrCast + sealOfVengeanceDmg + (righteousVengeanceDmg * crit) );
+		spellGcds.set(Ability.HammerOfWrath, gcd);
+	}
+	
+	if (document.getElementById("ExorcismEnabled").checked) {
+		abilitys.push(Ability.Exorcism);
+		spellCDs.set(Ability.Exorcism, document.getElementById("ExorcismCD").valueAsNumber * s);
+		spellDmgs.set(Ability.Exorcism, document.getElementById("ExorcismAvrCast").valueAsNumber);
+		spellGcds.set(Ability.Exorcism, document.getElementById("SpellGCD").valueAsNumber * s);
+	}
+	
+	if (document.getElementById("ConsecrationEnabled").checked) {
+		abilitys.push(Ability.Consecration);
+		spellCDs.set(Ability.Consecration, document.getElementById("ConsecrationCD").valueAsNumber * s);
+		let avrHit = document.getElementById("ConsecrationAvrHit").valueAsNumber;
+		let miss = document.getElementById("ConsecrationMiss").valueAsNumber / 100;
+		spellDmgs.set(Ability.Consecration, (avrHit - (avrHit * miss)) * (spellCDs.get(Ability.Consecration) / s));
+		spellGcds.set(Ability.Consecration, document.getElementById("SpellGCD").valueAsNumber * s);
+	}
+	
+	if (document.getElementById("HolyWrathEnabled").checked) {
+		abilitys.push(Ability.HolyWrath);
+		spellCDs.set(Ability.HolyWrath, document.getElementById("HolyWrathCD").valueAsNumber * s);
+		spellDmgs.set(Ability.HolyWrath, document.getElementById("HolyWrathAvrCast").valueAsNumber);
+		spellGcds.set(Ability.HolyWrath, document.getElementById("SpellGCD").valueAsNumber * s);
+	}
+	let startTime = document.getElementById("MinFightLength").valueAsNumber * s
+	let endTime = document.getElementById("MaxFightLength").valueAsNumber * s
+	let timeStep = document.getElementById("Timestep").valueAsNumber * s
+	
+	const prioWorker = new Worker("worker.js");
+	prioWorker.postMessage([abilitys, startTime, endTime, timeStep, spellCDs, spellDmgs, spellGcds]);
+	
+	prioWorker.onmessage = function(e) {
+		document.getElementById('OutputArea').value = e.data;
+	}
+	
+} 
---
+++ worker.js
@@ -0,0 +1,124 @@
+const Ability = {
+	Judgement: "Judgement",
+	CrusaderStrike: "CrusaderStrike",
+	DivineStorm: "DivineStorm",
+	HammerOfWrath: "HammerOfWrath",
+	Exorcism: "Exorcism",
+	Consecration: "Consecration",
+	HolyWrath: "HolyWrath"
+}
+
+const ms = 1
+const s = 1000
+const gcd = 1500
+
+onmessage = function(e) {
+	const abilitys = e.data[0];
+	const startTime = e.data[1];
+	const endTime = e.data[2];
+	const timeStep = e.data[3];
+	const spellCDs = e.data[4];
+	const spellDmgs = e.data[5];
+	const spellGcds = e.data[6];
+	
+	results = []
+	for (const prio of permutator(abilitys)) {
+		let avrPrioDmg = 0;
+		let runs = 0;
+		for (let i = startTime; i <= endTime; i+=timeStep) {
+	  		avrPrioDmg += runRotation(prio, spellCDs, spellDmgs, spellGcds, i);
+			runs++;
+		}
+		results.push([avrPrioDmg / runs, prio])
+	}
+	
+	results.sort(function(a, b){return b[0] - a[0]}); 
+	resultString = ""
+	for (const result of results){
+		resultString += result[0].toFixed(4) + ": [" + result[1].toString() + "]\n"; 
+	}
+	
+	postMessage(resultString);
+}
+
+function runRotation(prio, spellCDs, spellDmgs, spellGcds, fightTime) {
+	const currentSpellCds = new Map();
+	const spellUsedCount = new Map();
+	const spellLastCastAt = new Map();
+
+	for (const ability of prio) {
+		currentSpellCds.set(ability, 0)
+		spellUsedCount.set(ability, 0)
+		spellLastCastAt.set(ability, 0)
+	}
+	if(prio.includes(Ability.HammerOfWrath)){
+		currentSpellCds.set(Ability.HammerOfWrath, Math.trunc((fightTime * (1 - 0.2))));
+	}
+	
+	currentTime = 0;
+	
+	while(currentTime < fightTime){
+		let spellCast = null;
+		for (const ability of prio) {
+			if(currentSpellCds.get(ability) <= 0) {
+				spellUsedCount.set(ability, spellUsedCount.get(ability) + 1)
+				spellLastCastAt.set(ability, currentTime)
+				currentSpellCds.set(ability, spellCDs.get(ability))
+				spellCast = ability
+				break;
+			}
+		}
+		
+		
+		if(spellCast != null){
+			currentTime += spellGcds.get(spellCast)
+			subtractTimeFromCDs(currentSpellCds, spellGcds.get(spellCast))
+		}else{
+			let lowestCd = Math.min(...currentSpellCds.values())
+			if(lowestCd > 0){
+				currentTime += lowestCd
+				subtractTimeFromCDs(currentSpellCds, lowestCd)
+			}
+		}
+	}
+	
+	if(prio.includes(Ability.Consecration) && spellLastCastAt.get(Ability.Consecration) + spellCDs.get(Ability.Consecration) > fightTime){
+		let reduceCast = spellUsedCount.get(Ability.Consecration) - (1 - (fightTime - spellLastCastAt.get(Ability.Consecration)) / spellCDs.get(Ability.Consecration))
+		spellUsedCount.set(Ability.Consecration, reduceCast)
+	}
+	
+	let damage = 0
+	for (let ability of spellUsedCount.keys()) {
+		damage += spellUsedCount.get(ability) * spellDmgs.get(ability)
+	}
+		
+	return damage / (fightTime / s)
+}
+
+function subtractTimeFromCDs (currentSpellCds, time) {
+	for (let ability of currentSpellCds.keys()) {
+		currentSpellCds.set(ability, currentSpellCds.get(ability) - time)
+	}
+}
+
+
+function permutator(inputArr) {
+  var results = [];
+
+  function permute(arr, memo) {
+    var cur, memo = memo || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
